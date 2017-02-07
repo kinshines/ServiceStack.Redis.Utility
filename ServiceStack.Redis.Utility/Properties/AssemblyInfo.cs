2afe220851b3fb8815c22324fb4d1ab42336bb7f
kinshines <1176807665@qq.com>
2017-02-07 15:56:56 +0800

    assembly

--- ServiceStack.Redis.Utility/Properties/AssemblyInfo.cs
+++ ServiceStack.Redis.Utility/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("ServiceStack.Redis.Utility")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Redis Utility Using ServiceStack.Redis")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("HaiMaWan")]
 [assembly: AssemblyProduct("ServiceStack.Redis.Utility")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © HaiMaWan 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
